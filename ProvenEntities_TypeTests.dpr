0e2c199d5ce2027e1f6bf1fdee386e247dde640d
Chuck Crowell-Taynton <chuck@funkypuppy.com>
2026-04-02 16:52:03 -0500

    Clarification/Cleanup

--- ProvenEntities_TypeTests.dpr
+++ ProvenEntities_TypeTests.dpr
@@ -26,8 +26,13 @@ uses
   {Examples}
   CategorizingWithBuckets in 'Examples\Delphi\CategorizingWithBuckets.pas';
 
+{ TODO -oChuck -cMusing :
+I've noticed that the more I lean on my own definitions, the leaner the end binary becomes.
+The problem is that the more I lean on my own definitions, the more I seemingly gain capability.
+It seems odd to me that both of those statements feel True at the same time.}
+
 type
-  //Replace T with any compilable type.
+  //Replace T with any compilable type; or add your own tests below.
   T = Cardinal;
 
 begin
@@ -35,14 +40,14 @@ begin
   All you need to do to prove things out at runtime is to execute the binary produced by this project file.
   You're likely only going to get 1 of 2 results when running from the command line:
   1) Complete Failure: (Likely brought to you by everyone's favorite bug to track down!) Runtime error <X> at address <Y>, or
-  2) Complete Passing: after running the binary, you see "Tests Started...Tests Completed"
+  2) Complete Passing: After running the binary, you see "Tests Started...Tests Completed"
   }
-  Write('Tests Started...');
-  //If you want to cover more ground, just add your own values for T
-  //just make sure to keep it between the first and last "WriteLn" (that way we can be sure no errors could have occurred)
+  System.Write('Tests Started...');
+  //If you want to cover more ground, just copy/paste(the line of source code below that tests what you want to cover) and replace T with whatever type.
+  //just make sure to keep it between the first and last System.Write (that way you can be sure no errors could have occurred)
   PE.TypeTests.Types.AllTests.Exercise();
   PE.TypeTests.Buckets.TypeTests<T>.Exercise();
   PE.DomainTests.Buckets.DomainTests<T>.Exercise();
   CategorizingWithBuckets.OrganizingNumbers.ByOddsAndEvens();
-  Write('Tests Completed');
+  System.Write('Tests Completed');
 end.
